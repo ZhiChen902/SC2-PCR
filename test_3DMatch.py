8f3e4da9a2bc5357f9543ef47055b0ee14e2c30d
ZhiChen <54473539+ZhiChen902@users.noreply.github.com>
2022-05-12 21:38:04 +0800

    Update test_3DMatch.py

--- test_3DMatch.py
+++ test_3DMatch.py
@@ -174,7 +174,8 @@ if __name__ == '__main__':
 
     import os
     os.environ['CUDA_VISIBLE_DEVICES'] = config.CUDA_Devices
-
+    if not os.path.exists("./logs"):
+        os.makedirs("./logs")
     log_filename = f'logs/3DMatch-{config.descriptor}.log'
     logging.basicConfig(level=logging.INFO,
                         filename=log_filename,
